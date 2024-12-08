e6cf216b478bfc4ce470d74bcf3bf37dd37b3188
rmdhniki <rmdhniki18@gmail.com>
2024-12-09 01:44:16 +0700

    Lading Page - Menambahkan Asset & Card

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -240,6 +240,7 @@ export default function Example() {
     </div>
   </div>
 
+
   <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <a href="#">
       <img class="rounded-t-lg" src="/images/News1.png" alt="" />
