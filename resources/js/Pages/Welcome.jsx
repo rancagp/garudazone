7cbec7791a5b6ba62dcd6f0eb65b48d844960df5
rancagp <ranca632@gmail.com>
2024-12-11 20:12:21 +0700

    testing

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -318,8 +318,6 @@ export default function Example() {
             </div>
           </div>
 
-          {/* konten 3 */}
-
           <div className="overflow-hidden bg-white py-24 sm:py-32">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
               <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
