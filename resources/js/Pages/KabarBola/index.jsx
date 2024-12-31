0d0669468e707b94fbbfbbb993f6d8072e6d2359
Hedi Sukur <warcraftntt@gmail.com>
2024-12-31 15:59:15 +0700

    Menambahkan efek hover pada penomoran pagination

--- resources/js/Pages/KabarBola/index.jsx
+++ resources/js/Pages/KabarBola/index.jsx
@@ -85,7 +85,6 @@ export default function Match() {
 
             <div className="w-full">
                 <div className="flex flex-wrap justify-center p-6 gap-6">
-                    {/* Add your card elements here */}
                     {/* Card 1 */}
                     <div className="bg-white shadow-md rounded-md w-56 flex-shrink-0 group transition-transform duration-300 transform group-hover:scale-105">
                         <div className="overflow-hidden rounded-t-md">
@@ -266,66 +265,62 @@ export default function Match() {
                  </div>
              </div>
                     
-                
-            
-
             {/* Pagination under cards */}
-            <div className="flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
-                    <div>
-                        <nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-sm">
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                <span className="sr-only">Previous</span>
-                                <ChevronLeftIcon aria-hidden="true" className="size-5" />
-                            </a>
-                            <a
-                                href="#"
-                                aria-current="page"
-                                className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                1
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                2
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                3
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                3
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                4
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                5
-                            </a>
-                            <a
-                                href="#"
-                                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                            >
-                                <span className="sr-only">Next</span>
-                                <ChevronRightIcon aria-hidden="true" className="size-5" />
-                            </a>
-                        </nav>
+            <div className="w-full">
+                <div className="flex flex-wrap justify-center p-6 gap-6">
+                    {/* Pagination */}
+                    <div className="flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+                            <div>
+                                <nav aria-label="Pagination" className="isolate inline-flex -space-x-px rounded-md shadow-sm">
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        <span className="sr-only">Previous</span>
+                                        <ChevronLeftIcon aria-hidden="true" className="size-5" />
+                                    </a>
+                                    <a
+                                        href="#"
+                                        aria-current="page"
+                                        className="relative z-10 inline-flex items-center bg-red-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 hover:bg-red-700"
+                                    >
+                                        1
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        2
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        3
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        4
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        5
+                                    </a>
+                                    <a
+                                        href="#"
+                                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-red-200 focus:z-20 focus:outline-offset-0"
+                                    >
+                                        <span className="sr-only">Next</span>
+                                        <ChevronRightIcon aria-hidden="true" className="size-5" />
+                                    </a>
+                                </nav>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
