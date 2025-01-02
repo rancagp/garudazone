728525da82e7020bc7d9d0126c35ab20a7f1556d
Hedi Sukur <warcraftntt@gmail.com>
2025-01-02 18:56:48 +0700

    Update ukuran dan responsif

--- resources/js/Pages/KabarBola/index.jsx
+++ resources/js/Pages/KabarBola/index.jsx
@@ -1,4 +1,4 @@
-import Footer from "@/Components/Button"; 
+import Footer from "@/Components/Button";
 import {
     Disclosure,
     DisclosureButton,
@@ -105,13 +105,13 @@ export default function Match() {
 
             <div className="w-full">
                 <div className="flex flex-wrap justify-center p-6 gap-6">
-                    {/* Card 1 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 1 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/14/675d826f6b838/pssi-vietnam-_x600.jpg?token=2f634388119272750865ec65219a10d9"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -128,13 +128,13 @@ export default function Match() {
                         </div>
                     </div>
 
-                    {/* Card 2 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 2 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/14/675d69395fa9c/dsc08126-_x600.jpg?token=12f31af58b571e252d1044d8e6966a82"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -144,20 +144,19 @@ export default function Match() {
                             >
                                 <h3 className="font-bold text-lg mb-2 text-black group-hover:text-red-500 transition-colors duration-300">
                                     PSSI Gembira Kenaikan Peringkat Timnas Putri
-                                    Indonesia di FIFA
                                 </h3>
                             </a>
                             <p className="text-gray-600 mb-4">14/12/2024</p>
                         </div>
                     </div>
 
-                    {/* Card 3 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 3 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/14/675d69f636edb/day-1-11-_x600.jpg?token=65329ed10aa4da8dc01d62dfb21651bc"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -174,13 +173,13 @@ export default function Match() {
                         </div>
                     </div>
 
-                    {/* Card 4 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 4 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/15/675e559215ba2/whatsapp-image-2024-12-13-at-00-08-24-_x600.jpeg?token=5ac5607ade351fd893985baf9c6a822b"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -189,21 +188,21 @@ export default function Match() {
                                 className="bg-white shadow-md rounded-md"
                             >
                                 <h3 className="font-bold text-lg mb-2 text-black group-hover:text-red-500 transition-colors duration-300">
-                                    Erick Thohir Soroti Potensi Besar Esports:
-                                    Generasi Muda Jadi Kunci
+                                    Timnas Esports Indonesia Juara FIFAe World
+                                    Cup 2024 Kategori Console
                                 </h3>
                             </a>
                             <p className="text-gray-600 mb-4">14/12/2024</p>
                         </div>
                     </div>
 
-                    {/* Card 5 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 5 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/10/675865e1ca13d/ben00357-_x600.jpg?token=3f513ef130ce71e2ad1a3b98ff057816"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -221,13 +220,13 @@ export default function Match() {
                         </div>
                     </div>
 
-                    {/* Card 6 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 6 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/14/675d6899a0d2f/whatsapp-image-2024-12-14-at-16-07-09-_x600.jpeg?token=3742de6ebe5f477d0aff9bb78b56ef89"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -244,13 +243,13 @@ export default function Match() {
                         </div>
                     </div>
 
-                    {/* Card 7 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 7 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/11/67597fbe7ef8e/whatsapp-image-2024-12-11-at-18-05-11-_x600.jpeg?token=47f8017d8a59f5dd797caab5a889685a"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -267,13 +266,13 @@ export default function Match() {
                         </div>
                     </div>
 
-                    {/* Card 8 */}
-                    <div className="bg-white shadow-md rounded-md w-64 max-w-full flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
-                        <div className="overflow-hidden rounded-t-md">
+                    {/* Cards 8 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
                             <img
                                 src="https://www.pssi.org/files/uploads/news/image/2024/Dec/12/675a510806602/ysp08650-_x600.jpg?token=b331a7ef3c366649c948a4a373a92a6d"
                                 alt="Timnas Senior"
-                                className="rounded-t-md w-full h-48 sm:h-40 lg:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
                             />
                         </div>
                         <div className="p-4">
@@ -289,6 +288,29 @@ export default function Match() {
                             <p className="text-gray-600 mb-4">14/12/2024</p>
                         </div>
                     </div>
+
+                    {/* Cards 9 */}
+                    <div className="bg-white shadow-md rounded-lg w-80 sm:w-80 flex-shrink-0 group transition-transform duration-300 transform hover:scale-105">
+                        <div className="overflow-hidden rounded-t-lg">
+                            <img
+                                src="https://www.pssi.org/files/uploads/news/image/2024/Dec/12/675a50bbb8823/ysp08635-_x600.jpg?token=45b8caa8528e4d79da3d4d2d5877df02"
+                                alt="Timnas Senior"
+                                className="rounded-t-lg w-full h-56 sm:h-48 object-cover transform transition-transform duration-300 group-hover:scale-110"
+                            />
+                        </div>
+                        <div className="p-4">
+                            <a
+                                href="/kabar-bola/slug"
+                                className="bg-white shadow-md rounded-md"
+                            >
+                                <h3 className="font-bold text-lg mb-2 text-black group-hover:text-red-500 transition-colors duration-300">
+                                    Jelang Hadapi Laos, Shin Tae-yong Minta
+                                    Dukungan Garuda Fans
+                                </h3>
+                            </a>
+                            <p className="text-gray-600 mb-4">14/12/2024</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -297,7 +319,7 @@ export default function Match() {
                 <div className="flex flex-wrap justify-center p-6 gap-6">
                     {/* Pagination */}
                     <div className="flex items-center justify-center border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-                        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+                        <div className="flex sm:flex-1 sm:items-center sm:justify-center">
                             <div>
                                 <nav
                                     aria-label="Pagination"
@@ -362,6 +384,7 @@ export default function Match() {
                     </div>
                 </div>
             </div>
+
             {/* Footer */}
             <footer className="bg-gray-800 text-white py-4">
                 <div className="container mx-auto text-center">
