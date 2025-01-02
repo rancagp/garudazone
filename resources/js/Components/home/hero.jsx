e47c82c0a98b9366946576ae004669352bd256bd
Fakihelmi <helmifakih5@gmail.com>
2025-01-02 23:55:28 +0700

    ui home

--- resources/js/Components/home/hero.jsx
+++ resources/js/Components/home/hero.jsx
@@ -367,56 +367,56 @@ const Hero = () => {
                 </div>
             </div>
         </div>
-
         <div className="overflow-hidden bg-white py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                    <div className="lg:pr-8 lg:pt-4">
-                        <div className="lg:max-w-lg">
-                            <h2 className="text-base/7 font-semibold text-red-700">
-                                SEMUA MOMEN PENTING
-                            </h2>
-                            <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                                MOMEN PENTING TERBARU
-                            </p>
-                            <p className="mt-6 text-lg/8 text-gray-600">
-                                Lorem ipsum, dolor sit amet
-                                consectetur adipisicing elit.
-                                Maiores impedit perferendis suscipit
-                                eaque, iste dolor cupiditate
-                                blanditiis ratione.
-                            </p>
-                            <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                                {features.map((feature) => (
-                                    <div
-                                        key={feature.name}
-                                        className="relative pl-9"
-                                    >
-                                        <dt className="inline font-semibold text-gray-900">
-                                            <feature.icon
-                                                aria-hidden="true"
-                                                className="absolute left-1 top-1 size-5 text-indigo-600"
-                                            />
-                                            {feature.name}
-                                        </dt>{" "}
-                                        <dd className="inline">
-                                            {feature.description}
-                                        </dd>
-                                    </div>
-                                ))}
-                            </dl>
-                        </div>
-                    </div>
-                    <img
-                        alt="Product screenshot"
-                        src="/images/timnaswp4.jpg"
-                        width={2432}
-                        height={1442}
-                        className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                    />
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div className="lg:pr-8 lg:pt-4 hover:scale-105 transform transition-transform duration-300">
+                <div className="lg:max-w-lg">
+                    <h2 className="text-base/7 font-semibold text-red-700">
+                        SEMUA MOMEN PENTING
+                    </h2>
+                    <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                        MOMEN PENTING TERBARU
+                    </p>
+                    <p className="mt-6 text-lg/8 text-gray-600">
+                        Lorem ipsum, dolor sit amet
+                        consectetur adipisicing elit.
+                        Maiores impedit perferendis suscipit
+                        eaque, iste dolor cupiditate
+                        blanditiis ratione.
+                    </p>
+                    <dl className="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                        {features.map((feature) => (
+                            <div
+                                key={feature.name}
+                                className="relative pl-9"
+                            >
+                                <dt className="inline font-semibold text-gray-900">
+                                    <feature.icon
+                                        aria-hidden="true"
+                                        className="absolute left-1 top-1 size-5 text-indigo-600"
+                                    />
+                                    {feature.name}
+                                </dt>{" "}
+                                <dd className="inline">
+                                    {feature.description}
+                                </dd>
+                            </div>
+                        ))}
+                    </dl>
                 </div>
             </div>
+            <img
+                alt="Product screenshot"
+                src="/images/timnaswp4.jpg"
+                width={2432}
+                height={1442}
+                className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0 hover:scale-105 transform transition-transform duration-300"
+            />
         </div>
+    </div>
+</div>
+
 
         {/* footer */}
         <footer className="bg-gray-800 text-white py-4">
